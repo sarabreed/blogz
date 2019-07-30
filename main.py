d589a0441a2b8441b9f18bbfd986ed68ad2b3d23
Sara Breed <sara.n..breed@gmail.com>
2019-07-30 07:24:51 -0500

    Updated Blogz through Home Page

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import cgi
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
 
 #mysql+pymysql://username:password@port/database
 
@@ -16,44 +16,149 @@ db = SQLAlchemy(app)
 
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RU'
 
+# TO DO: Add a User class to make all this new 
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(50), unique = True)
+    password = db.Column(db.String(50))
+    blogs = db.relationship('Blog', backref='owner')
+    
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
 #creating a persistent class that can be put in the database.
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.Text)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 #look at flick-list as well
 #this sets the default value
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
 
+# this will run for every request
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'signup', 'blogs', 'index', 'post']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+        return redirect ('/login')
+
+#display the login page
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+
+        if not user:
+            flash("Yikes! This username does not exist.", 'error')
+            return redirect ('/login') 
+
+        if user and user.password == password:
+            #remembers that the user has logged in
+            session['username'] = username
+            flash("You are Logged In.  Welcome back, "+user.username)
+            return redirect ('/newpost')   
+        elif user.password != password:
+            flash("Whoopsies! That password is incorrect. Try again.", 'error') #  explain why the login failed
+            return redirect ('/login')       
+        
+    return render_template('login.html')
+
+#displa signup and validate signup data
+@app.route('/signup', methods = ['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form ['verify']
+        existing_user = User.query.filter_by(username=username).first()
+        errors = False  
+
+        if username == '' or password == '' or verify == '':
+            flash('One or more fields are invalid. All fields must be complete', 'error')
+            errors = True
+
+        elif existing_user and existing_user.username == username:
+            flash('Yikes! That username already exists.', 'error')
+            errors = True
+
+        elif len(username) <3:
+            flash('INVALID USERNAME - Come on man, the username has to be more than 3 characters!', 'error')
+            errors = True
+
+        elif len(password) <3:
+            flash('INVALID PASSWORD - Get it together! Your password has to be more than 3 characters!', 'error')
+            errors = True
+        
+        elif password != verify:
+            flash('PASSWORDS DO NOT MATCH - Hey, fat fingers. Try again!', 'error') 
+            errors = True
+
+        if errors:
+            return redirect('/signup')
+
+        if not existing_user:
+            new_user = User(username,password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            return redirect('/newpost')
+      
+
+    return render_template('signup.html')
+
+@app.route('/logout')
+def logout():
+    del session['username']
+    return redirect ('/blog')
+
+
+@app.route('/', methods = ['POST', 'GET'])
+def index():
+    
+    users= User.query.all()
+
+    return render_template('index.html', title= "Blogz!", users=users)
 
 @app.route('/blog', methods = ['POST', 'GET'])
-def index():
+def blogs():
+    # owner = User.query.filter_by(username=session['username']).first()
+    # if request.method == 'GET':
+    #     
+    #     blogs = Blog.query.filter_by(owner=owner).all()
+
+    #     return render_template('singleuser.html', title = "Blogz!", blogs = blogs)
     
     blogs= Blog.query.all()
 
-    return render_template('index.html', title= "Build a Blog!", blogs=blogs)
+    return render_template('blogs.html', title= "Blogz!", blogs = blogs)
   
 
 @app.route('/newpost', methods = ['POST', 'GET'])
 def newpost():
 
     if request.method == 'GET':
-        return render_template('addentry.html', title= "Build a Blog!")
+        return render_template('addentry.html', title= "Blogz!")
 
     elif request.method == 'POST':
         blog_title = request.form['blog_title']
         blog_body = request.form['blog_body']
-        
-        errors = False
+        owner = User.query.filter_by(username=session['username']).first()
+        blog = Blog(blog_title,blog_body, owner)
 
-        # if blog_title =='' and blog_body =='':
-        #     flash ("Uh oh! You forgot to do anything with this post", 'error')
-        #     #return redirect('/newpost')
-        #     errors = True
+        errors = False
         
         if blog_title == '' and blog_body == '':
             flash("Everything is blank! Try again.", 'error')
@@ -61,24 +166,36 @@ def newpost():
         
         elif blog_title =='':
             flash("Yikes! You forgot to add a title to your blog post!",'error')
-            #return redirect('/newpost')
             errors = True
         elif blog_body == '':
             flash("Whoops! You need to write your post here.",'error')
-            #return redirect('/newpost')
             errors = True
 
         if errors:
-            return redirect('/newpost')
+            return redirect('/newpost')       
         
-            
-
-        blog = Blog(blog_title,blog_body)
         db.session.add(blog)
         db.session.commit()
 
     return render_template('post.html', blog= blog)
         
+@app.route('/user')
+def user_posts():
+
+    # owner= User.query.filter_by(username = ['username']).all()
+
+    # blogs = Blog.query.filter_by(owner=owner).all()
+#     # user_id = request.args.get('user_id')
+
+    return render_template('singleuser.html')
+
+# @app.route('/post')
+# def post(blog_id):
+
+#     blog= Blog.query.filter_by(id=blog_id).one()
+#     blog_id = request.args.get('blog_id')
+
+#     return redirect("/post?blog_id")
 
 @app.route('/post/<int:blog_id>')
 def post(blog_id):
@@ -86,44 +203,9 @@ def post(blog_id):
     blog= Blog.query.filter_by(id=blog_id).one()
 
     return render_template('post.html', blog = blog)
+
+
   
-# @app.route('/post/<int:post_id>')
-# def post(post_id):
-#     post = Blogpost.query.filter_by(id=post_id).one()
-
-#     return render_template('post.html', post=post)
-
-    # 
-    # return render_template('addentry.html',title="Build a Blog", 
-    #     title = title, body=body)
-
-
-# @app.route('/blog', methods = ['POST'])
-# def validate_fields():
-#     title = request.addentry['title']
-#     body = request.addentry['body']
-
-
-#     title_error = ''
-#     body_error = ''
-
-#     if title == '':
-#         title_error = "You forgot to add a title to your blog post."
-#         title = ''
-    
-#     if body == '':
-#         body_error ="Whoops! You need to write your post here."
-#         body = ''
-
-#     if title_error != '' or body_error != '':
-
-#         return render_template('addentry.html', title = title, title_error = title_error, body = body, body_error=body_error)
-
-#     else:
-#         return "YES"
-
-
-
 
 if __name__ == '__main__':
     # this gets added because of the import of the database
